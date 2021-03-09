977c8dbb763ec3fa50a8b37c5e08e3ba628bff07
DenKozlov04 <datlovdenis363@gmail.com>
2021-03-09 15:39:02 +0200

    15 lesson

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-bookName.textContent = value;
+const list = document.querySelector('#book-list ul');
+const forms = document.forms;
+
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
+
+// add books
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  bookName.textContent = value;
   deleteBtn.textContent = 'delete';
 
   // add classes
@@ -9,5 +32,15 @@ bookName.textContent = value;
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
   list.appendChild(li);
-  //list.insertBefore(li, list.querySelector('li:first-child'));
 });
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
+});
+© 2021 GitHub, Inc.
